ed21dee667b5cef0512800acbb4f32328042dcf9
khaled gabr <khaledgabr77@gmail.com>
2020-07-19 11:30:12 +0200

    add package with missing dep and try to fix error

---
+++ Day3/catkin_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/khaled/auc/Day3/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export PATH="/opt/ros/kinetic/bin:/home/khaled/rob599/scripts:/home/khaled/anaconda3/bin:/home/khaled/bin:/home/khaled/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/opt/ParaView-5.2.0-Qt4-OpenGL2-MPI-Linux-64bit/bin/"
+export PWD="/home/khaled/auc/Day3/catkin_ws/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/khaled/auc/Day3/catkin_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/khaled/auc/Day3/catkin_ws/src:$ROS_PACKAGE_PATH"
