c52e9cf29474f1957e8f6859a20e057f80e39d52
khaled gabr <khaledgabr77@gmail.com>
2020-07-20 11:36:04 +0200

    workspace

--- Day4 /catkin_ws/src/light_robot/scripts/Publish.py
+++ Day4 /catkin_ws/src/light_robot/scripts/Publish.py
@@ -1,12 +1,14 @@
-#!/usr/bin/env python
-# license removed for brevity
+#!/usr/bin/env python         
+
 import rospy
 from std_msgs.msg import String
 
 def talker():
+
     pub = rospy.Publisher('topic_1', String, queue_size=10)
     rospy.init_node('node_1', anonymous=False)
     rate = rospy.Rate(10) # 10hz
+
     while not rospy.is_shutdown():
         hello_str = "hello world  " #% rospy.get_time()
         rospy.loginfo(hello_str)
