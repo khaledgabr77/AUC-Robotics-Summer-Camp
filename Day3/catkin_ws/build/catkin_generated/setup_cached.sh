e71d99f5583978169f98d1f90ee7ba3133498344
khaled gabr <khaledgabr77@gmail.com>
2020-08-14 04:35:50 +0200

    editing pkg

--- Day3/catkin_ws/build/catkin_generated/setup_cached.sh
+++ Day3/catkin_ws/build/catkin_generated/setup_cached.sh
@@ -8,7 +8,7 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/khaled/auc/Day3/catkin_ws/devel:$CMAKE_PREFIX_PATH"
-export PATH="/opt/ros/kinetic/bin:/home/khaled/rob599/scripts:/home/khaled/anaconda3/bin:/home/khaled/bin:/home/khaled/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/opt/ParaView-5.2.0-Qt4-OpenGL2-MPI-Linux-64bit/bin/"
-export PWD="/home/khaled/auc/Day3/catkin_ws/build"
-export ROSLISP_PACKAGE_DIRECTORIES="/home/khaled/auc/Day3/catkin_ws/devel/share/common-lisp"
+export LD_LIBRARY_PATH='/home/khaled/auc/Day3/catkin_ws/devel/lib:/opt/ros/melodic/lib:/lib64:/usr/lib/x86_64-linux-gnu/'
+export PWD='/home/khaled/auc/Day3/catkin_ws/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/home/khaled/auc/Day3/catkin_ws/devel/share/common-lisp'
 export ROS_PACKAGE_PATH="/home/khaled/auc/Day3/catkin_ws/src:$ROS_PACKAGE_PATH"
